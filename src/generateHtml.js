b2f8eae9bdc85cf0d14fe7d28984e1a10f41dc91
Natalia Venkatesh <nataliavenkatesh@MacBook-Pro.local>
2022-11-23 06:14:11 -0500

    corrected HTML

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -12,37 +12,42 @@ const generateHTML = (team) =>
 <body>
 
 
-${team.map(employee => 
-  `<div>
-  <h2>Name: ${employee.name}</h2>
-  <h3>${employee.getRole()}</h3>
-
-  
- 
-   ${if(employee.getRole() === "Manager"){
-    return <h3>ID: ${employee.id}</h3>
+${team.map(employee => {
+ if(employee.getRole() === "Manager"){
+    return `<div>
+    <h2>Name: ${employee.name}</h2>
+          <h3>${employee.getRole()}</h3>
+          <h3>ID: ${employee.id}</h3>
           <h3>Email: ${employee.email}</h3>
           <h3>Office: ${employee.office}</h3>
-    },
-  }
-   ${ else if(employee.getRole() === "Engineer"){
-      return <h3>ID: ${employee.id}</h3>
-      <h3>Email: ${employee.email}</h3>
-      <h3>GitHub: ${employee.github}</h3>;
-    },
-  }
-    ${else if(employee.getRole() === "Intern"){
-      return <h3>ID: ${employee.id}</h3>
-      <h3>Email: ${employee.email}</h3>
-      <h3>GitHub: ${employee.school}</h3>;
-    },
-  }
+          </div>`
+    }
+    
 
-
+  if(employee.getRole() === "Engineer"){
+      return `<div>
+      <h2>Name: ${employee.name}</h2>
+            <h3>${employee.getRole()}</h3>
+            <h3>ID: ${employee.id}</h3>
+            <h3>Email: ${employee.email}</h3>
+            <h3>GitHub: ${employee.github}</h3>
+            </div>`
+      }
+  else if(employee.getRole() === "Intern"){
+        return `<div>
+        <h2>Name: ${employee.name}</h2>
+              <h3>${employee.getRole()}</h3>
+              <h3>ID: ${employee.id}</h3>
+              <h3>Email: ${employee.email}</h3>
+              <h3>School: ${employee.school}</h3>
+              </div>`
+        }
+      })   
+ .join("")
+    }
   
-  </div>`
-)
-.join("")}
+  
+
 
 </body>
 </html>`;
