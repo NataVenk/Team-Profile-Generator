db676b4698c2728d14b238e9f3384dc6af2a3ab4
Natalia Venkatesh <nataliavenkatesh@MacBook-Pro.local>
2022-11-23 09:52:33 -0500

    readme and video added

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,6 +1,6 @@
 
 const generateHTML = (team) =>
-`<!DOCTYPE html>
+  `<!DOCTYPE html>
   
 <html lang="en">
 <head>
@@ -11,43 +11,54 @@ const generateHTML = (team) =>
 </head>
 <body>
 
+<div class="jumbotron jumbotron-fluid bg-dark text-white">
+  <div class="container bg-dark text-white">
 
-${team.map(employee => {
- if(employee.getRole() === "Manager"){
-    return `<div>
-    <h2>Name: ${employee.name}</h2>
-          <h3>${employee.getRole()}</h3>
-          <h3>ID: ${employee.id}</h3>
-          <h3>Email: ${employee.email}</h3>
-          <h3>Office: ${employee.office}</h3>
+    <h1 class="display-4 "> My Team</h1>
+    
+  </div>
+</div>
+<div class="d-flex justify-content-around"> 
+  ${team.map(employee => {
+    if (employee.getRole() === "Manager") {
+      return `<div class="card card-body d-flex justify-content-around " style="width: 18rem">
+    <h2 class="card-title bg-secondary text-white"> Name: ${employee.name}</h2>
+          <h3 class="text-primary">${employee.getRole()}</h3>
+          <h3 class="list-group-item bg-light text-dark" > ID: ${employee.id}</h3>
+          <h3 class="list-group-item bg-light text-dark" > Email: 
+                <a href="mailto:${employee.email}">${employee.email}</a></h3>
+          <h3 class="list-group-item bg-light text-dark" > Office: ${employee.office}</h3>
           </div>`
     }
-    
 
-  if(employee.getRole() === "Engineer"){
-      return `<div>
-      <h2>Name: ${employee.name}</h2>
-            <h3>${employee.getRole()}</h3>
-            <h3>ID: ${employee.id}</h3>
-            <h3>Email: ${employee.email}</h3>
-            <h3>GitHub: ${employee.github}</h3>
+
+    if (employee.getRole() === "Engineer") {
+      return `<div class="card card-body d-flex justify-content-around" style="width: 18rem">
+      <h2 class="card-title bg-secondary text-white"> Name: ${employee.name}</h2>
+            <h3 class="text-primary">${employee.getRole()}</h3>
+            <h3 class="list-group-item bg-light text-dark" > ID: ${employee.id}</h3>
+            <h3 class="list-group-item bg-light text-dark" > Email: 
+                <a href="mailto:${employee.email}">${employee.email}</a></h3>
+            <h3 class="list-group-item bg-light text-dark" > GitHub:
+                <a href="https://github.com/${employee.github}">${employee.github}</a></h3>
             </div>`
-      }
-  else if(employee.getRole() === "Intern"){
-        return `<div>
-        <h2>Name: ${employee.name}</h2>
-              <h3>${employee.getRole()}</h3>
-              <h3>ID: ${employee.id}</h3>
-              <h3>Email: ${employee.email}</h3>
-              <h3>School: ${employee.school}</h3>
-              </div>`
-        }
-      })   
- .join("")
     }
+    else if (employee.getRole() === "Intern") {
+      return `<div class="card card-body d-flex justify-content-around" style="width: 18rem">
+        <h2 class="card-title bg-secondary text-white"> Name: ${employee.name}</h2>
+              <h3 class="text-primary">${employee.getRole()}</h3>
+              <h3 class="list-group-item bg-light text-dark" > ID: ${employee.id}</h3>
+              <h3 class="list-group-item bg-light text-dark" > Email: 
+                <a href="mailto:${employee.email}">${employee.email}</a></h3>
+              <h3 class="list-group-item bg-light text-dark" > School: ${employee.school}</h3>
+              </div>`
+    }
+  })
+    .join("")
+  }
   
   
-
+</div>
 
 </body>
 </html>`;
@@ -55,4 +66,3 @@ ${team.map(employee => {
 module.exports = generateHTML
 
 
- 
